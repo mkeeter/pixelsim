93b7dfccdc24ec589984403b75fe1440b2e0aa0a
Matt Keeter <matt.j.keeter@gmail.com>
2013-11-17 15:24:15 -0500

    Simulation is running

--- ship.cc
+++ ship.cc
@@ -441,8 +441,8 @@ void Ship::MakeTextures()
         size_t i=0;
         for (size_t y=0; y <= height; ++y) {
             for (size_t x=0; x <= width; ++x) {
-                pos[i++] = x+ ((rand() % 100) - 50) / 200.;
-                pos[i++] = y + ((rand() % 100) - 50) / 200.;
+                pos[i++] = x;
+                pos[i++] = y;
             }
         }
 
