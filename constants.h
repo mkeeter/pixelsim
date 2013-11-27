105455159a5b015648b31f3e46ac121a94113d3d
Matt Keeter <matt.j.keeter@gmail.com>
2013-11-27 00:04:31 -0500

    Missing include file

---
+++ constants.h
@@ -0,0 +1,22 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+// Thrust engine
+#define SHIP_ENGINE_THRUST_R    255
+#define SHIP_ENGINE_THRUST_G    0
+#define SHIP_ENGINE_THRUST_B    0
+#define SHIP_ENGINE_THRUST      2
+
+// Left engine
+#define SHIP_ENGINE_RIGHT_R     255
+#define SHIP_ENGINE_RIGHT_G     0
+#define SHIP_ENGINE_RIGHT_B     1
+#define SHIP_ENGINE_RIGHT       3
+
+// Right engine
+#define SHIP_ENGINE_LEFT_R      255
+#define SHIP_ENGINE_LEFT_G      0
+#define SHIP_ENGINE_LEFT_B      2
+#define SHIP_ENGINE_LEFT        4
+
+#endif
